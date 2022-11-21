6559e0187575ee3ddbdbe4be6d0821651c3a6a3d
Akshay <akshayksama@gmail.com>
2022-11-21 21:16:31 +0530

    Updates

---
+++ precache-manifest.66fdb25c6036060715ce58f730995d6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e58df6632047d347584a",
+    "url": "/Netflix/static/css/main.d05d56dc.chunk.css"
+  },
+  {
+    "revision": "e58df6632047d347584a",
+    "url": "/Netflix/static/js/main.e58df663.chunk.js"
+  },
+  {
+    "revision": "71fc4e3b0b7ccd07314e",
+    "url": "/Netflix/static/js/1.71fc4e3b.chunk.js"
+  },
+  {
+    "revision": "2d300fbb7525717308e2",
+    "url": "/Netflix/static/js/runtime~main.2d300fbb.js"
+  },
+  {
+    "revision": "078d39a9778ac1fab4cec4f486ff59a2",
+    "url": "/Netflix/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdb431bc34a0f4265f9994397cad461d.js"
+  "/Netflix/precache-manifest.66fdb25c6036060715ce58f730995d6b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Netflix/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
